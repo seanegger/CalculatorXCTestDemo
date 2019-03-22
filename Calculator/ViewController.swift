0d78cb961d1bdc404885adf263e2034717c4fd22
Sean Egger <seanegger@seans-macbook-pro.local>
2019-03-22 12:46:05 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Sean Egger on 3/22/19.
+//  Copyright © 2019 Sean Egger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
